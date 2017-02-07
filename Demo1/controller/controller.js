cab9f87ed8b9cf22af3d9f8f0cefac80f848c5d2
PiyushGK <kawadepiyush394@gmail.com>
2017-02-08 00:02:34 +0530

    test

--- Demo1/controller/controller.js
+++ Demo1/controller/controller.js
@@ -1,12 +1,57 @@
-module.controller('homeCtrl' , ['$scope', 'homeService', function ($scope, homeService) {
-	$scope.name = 'Piyush Kawade'
+module.controller('homeCtrl' , ['$scope', 'homeService','$http', function ($scope, homeService, $http) {
+	/*$scope.name = 'Piyush Kawade'
 	$scope.empID = '1241841';
 	$scope.companyName = 'TCS';
 
-	
 
 	$scope.saveData = function (inputData){
 		homeService.getData(inputData);
 	}
+	 
+	$http.get("./json/info.json").then(function (response){
+		$scope.dk = response.data;
+	})
+*/
+	var uid = 1;
 
+	$scope.contacts=[{
+		'id' : 0,
+		'name':'Piyush',
+		'email':'Kawadepiyush@gamil.com',
+		'phoneNo':'9689656986'
+	}];
+
+	$scope.saveContact = function(){
+		if ($scope.newContact.id == null) {
+			$scope.newContact.id = uid++ ;
+			$scope.contacts.push($scope.newContact);
+		}
+		else{
+			for(i in $contacts){
+				$scope.contacts[i].id == $scope.newContact[i].id ;
+				$scope.contacts[i] == $scope.newContact ;
+			}
+		}
+	}
+	$scope.newContact = {};
+
+	$scope.deleteContact = function(id) {
+		for(i in $scope.contacts){
+			if($scope.contacts[i].id == id){
+				$scope.contacts.splice(i ,1);
+				$scope.newContact = {};
+			}
+		}
+	}
+
+	$scope.editContact =function (id){
+		for(i in $scope.contacts){
+			if($scope.contacts[i].id == id) {
+            	//we use angular.copy() method to create 
+            	//copy of original object
+                $scope.newcontact = angular.copy($scope.contacts[i]);
+            }
+		}
+	}
+	
 }]);
--- Demo1/controller/service.js
+++ Demo1/controller/service.js
@@ -1,5 +1,31 @@
-module.service('homeService', function () {
+module.service('homeService', function ($http, $q) {
 	this.getData = function(test){
 		console.log('welcome' + test);
 	}
+
+this.callData = function(){
+	var data = [
+	{name:'Dhan', companyName:'tcs'},
+	{name:'Piyush', companyName:'tata'}
+	]
+	return data;
+}
+/*this.newData = function(){
+	var deffered = $q.defer();
+	 $http.get("./json/info.json").then(function (response){		 
+		deffered.resolve(response.data);
+	}, function(err){
+		deffered.reject(err)
+	})
+	return deffered.promise;
+}*/
+
+/*this.newData = function(){
+  $http.get("./json/info.json").then(function (response){
+		return response;
+	})
+
+};*/
+
+
 })
